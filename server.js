b4a8f34eafb43a920d196d74789926d8ce7d78d8
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-17 23:53:08 -0700

    http server:

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-import config, { nodeEnv } from './config';
+import http from 'http';
 
-console.log(config, nodeEnv);
+
+const server = http.createServer((req, res) => {
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can stream!!!\n');
+    res.end();
+  }, 3000)
+});
+
+server.listen(8080);
+
+
